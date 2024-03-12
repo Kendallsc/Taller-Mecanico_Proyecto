95c7acbc009b5a720d319691c611b06b3c13860e
Kendsc <105095783+Kendsc@users.noreply.github.com>
2024-03-12 00:14:15 -0600

    Se agrega el proyecto

---
+++ src/main/java/com/mycompany/taller/mecanico_proyecto/TallerMecanico_Proyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller.mecanico_proyecto;
+
+/**
+ *
+ * @author Kendall
+ */
+public class TallerMecanico_Proyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
